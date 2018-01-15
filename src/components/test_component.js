266e2e4090038c7e18fdfe5a6e9197b4e840a109
bogdan <bogdan@bogdans-MacBook-Pro.local>
2018-01-15 23:41:24 +0100

    Add a test component and render a simple message. That's all for today :)

---
+++ src/components/test_component.js
@@ -0,0 +1,13 @@
+// Import react
+import React, { Component } from 'react';
+
+// Create a class component
+class Test extends Component {
+    // Render some message
+    render() {
+        return <h1>Bogdan Bugariu Frontend Test</h1>;
+    }
+}
+
+// Export the component
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-const API_KEY = "AIzaSyB_ZhZ8jZ5P55hcDITtsXJioF871M8CWtQ";
+import Test from './components/test_component';
 
 // Create a component that generate some html code
 class App extends Component {
     render () {
         return (
-            <h1>
-                Bogdan Bugariu Frontend Test
-            </h1>
+            <div>
+                <Test />
+            </div>
         );
     }
 }
